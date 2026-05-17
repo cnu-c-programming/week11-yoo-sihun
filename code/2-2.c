448cf3f04f6cfa1b789f823a51bd5ebbcac95e0b
yosi475 <yosi475@naver.com>
2026-05-17 13:37:00 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -4,12 +4,13 @@ enum Color { RED, GREEN, BLUE };
 enum Status { OK = 0, ERR = -1, BUSY = 1 };
 
 int main(void) {
-    printf("RED = %d\n", RED);
+    // = 기호의 위치를 맞추기 위해 공백(띄어쓰기) 추가
+    printf("RED   = %d\n", RED);
     printf("GREEN = %d\n", GREEN);
-    printf("BLUE = %d\n\n", BLUE);
+    printf("BLUE  = %d\n\n", BLUE);
 
-    printf("OK = %d\n", OK);
-    printf("ERR = %d\n", ERR);
-    printf("BUSY = %d\n", BUSY);
+    printf("OK    = %d\n", OK);
+    printf("ERR   = %d\n", ERR);
+    printf("BUSY  = %d\n", BUSY);
     return 0;
 }
